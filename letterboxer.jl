e419bbf796184ec5ce44a938def5b40ad79ec4f7
SpookyB <46918278+SpookyB@users.noreply.github.com>
2023-10-10 07:45:09 -0500

    Update letterboxer.jl
    Simpler types, better recursion, threading

--- letterboxer.jl
+++ letterboxer.jl
@@ -14,48 +14,39 @@ function enumerate_from(iter, start, step=1)
     zip(Iterators.countfrom(start, step), iter)
 end
 
-### constructors
-
-struct Puzzle
-    positions::Dict{UInt32, UInt32}
-    letters::UInt32
-    function Puzzle(string::String, side_length::UInt32)
-        trail = az_trail(string)
-        positions = Dict(letter => index÷side_length
-            for (index, letter) in enumerate_from(trail, side_length))
-        return new(positions, reduce(|, trail))
-    end
-end
+### types
 
 struct Word
-    index::UInt32
+    text::String
     last::UInt32
     letters::UInt32
-    function Word(trail::Array{UInt32}, index::UInt32)
-        return new(index, trail[end], reduce(|, trail))
-    end
 end
 
-struct Solution
-    prev::Union{Solution,Nothing}
-    index::UInt32
-    last::UInt32
+struct Proposal
+    prev::Union{Proposal,Nothing}
+    word::Word
     letters::UInt32
-    function Solution(solution::Solution, word::Word)
-        letters = solution.letters | word.letters
-        return new(solution, word.index, word.last, letters)
+    function Proposal(word::Word)
+        return new(nothing, word, word.letters)
     end
-    function Solution(word::Word)
-        return new(nothing, word.index, word.last, word.letters)
+    function Proposal(proposal::Proposal, word::Word)
+        return new(proposal, word, proposal.letters | word.letters)
     end
 end
 
 ### logic
 
-function walkable(puzzle, trail)
+function puzzle(string::String, side_length::UInt32)
+    trail = az_trail(string)
+    positions = Dict(letter => index÷side_length
+        for (index, letter) in enumerate_from(trail, side_length))
+    return positions, reduce(|, trail)
+end
+
+function walkable(positions, trail)
     last = 0
     for letter in trail
-        current = get(puzzle.positions, letter, 0)
+        current = get(positions, letter, 0)
         if (current == last) | (current == 0)
             return false
         end
@@ -64,39 +55,37 @@ function walkable(puzzle, trail)
     return true
 end
 
-function print_solutions(puzzle, dictionary, maximum_solution_size)
-
+function build_words(positions, dictionary)
     words = Dict()
-    solutions = []
-    for (index, entry) in enumerate(dictionary)
+    for entry in dictionary
         trail = az_trail(entry)
-        if walkable(puzzle, trail)
-            word = Word(trail, UInt32(index))
+        if walkable(positions, trail)
+            word = Word(entry, trail[end], reduce(|, trail))
             words[trail[1]] = [get(words, trail[1], []); word]
-            solutions = [solutions; Solution(word)]
         end
     end
+    return words
+end
 
-    for solution_size in range(1, maximum_solution_size)
+function print_proposal(io, proposal)
+    text = ""
+    while proposal != nothing
+        text = "$(proposal.word.text) $text"
+        proposal = proposal.prev
+    end
+    println(io, text)
+end
 
-        if solution_size != 1
-            solutions = Iterators.flatten(
-                [Solution(solution, word) for word in words[solution.last]]
-                    for solution in solutions)
+function print_solutions(io, required, words, proposal, depth) 
+
+    if required == proposal.letters
+        print_proposal(io, proposal)
+    end
+
+    if depth > 1
+        for word in words[proposal.word.last]
+            print_solutions(io, required, words, Proposal(proposal, word), depth-1)
         end
-
-        println("===$(solution_size) word solutions===")
-        for solution in solutions
-            if puzzle.letters == solution.letters
-                text = ""
-                while solution != nothing
-                    text = "$(dictionary[solution.index]) $text"
-                    solution = solution.prev
-                end
-                println(text)
-            end
-        end
-
     end
 
 end
@@ -123,7 +112,7 @@ function parse_commandline()
             arg_type = UInt32
             default = UInt32(3)
         "-d", "--dictionary"
-            help = "path to dictionary_path"
+            help = "path to dictionary"
             arg_type = String
             dest_name = "dictionary_path"
             default = "./resources/dictionary.txt"
@@ -137,14 +126,25 @@ function parse_commandline()
             arg_type = UInt32
             dest_name = "maximum_solution_size"
             default = UInt32(3)
+        "-o", "--out"
+            help = "path to output file (disables stdout)"
+            arg_type = String
+            dest_name = "output_path"
     end
     return parse_args(s)
 end
 
 function main(opt)
-    puzzle = Puzzle(opt["letters"], opt["side_length"])
+    positions, required = puzzle(opt["letters"], opt["side_length"])
     dictionary = load_dictionary(opt["dictionary_path"], opt["minimum_word_length"])
-    print_solutions(puzzle, dictionary, opt["maximum_solution_size"])
+    words = build_words(positions, dictionary)
+    io = isnothing(opt["output_path"]) ? stdout : open(opt["output_path"], "w")
+    for letter in values(words)
+        Threads.@threads for word in letter
+            print_solutions(io, required, words, Proposal(word), opt["maximum_solution_size"])
+        end
+    end
+    close(io)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
